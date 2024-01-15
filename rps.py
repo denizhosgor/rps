690b61a6d2b0f8c50c7d59f59d1018d2e933c652
Deniz Hoşgör <denizhosgor@gmail.com>
2024-01-15 14:45:17 +0300

    Update rps.py

--- rps.py
+++ rps.py
@@ -28,21 +28,26 @@ scissors = ("""
 """)
 
 list = [rock,paper,scissors]
+
 list_rand_num = random.randint(0,len(list)-1)
-selection = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if selection > 2:
-    print("You lose :(")
+input_sel = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+if not input_sel.isdigit():
+    if not isinstance(input_sel,int):
+        print("You can not use letter only numbers.\n")
 else:
-    print(list[selection])
+    selection = int(input_sel)
+    if int(input_sel) > 2:
+        print("You have only 3 selection not other numbers.\n")
+    else:
+        print(list[selection])
 
-    print("Computer chose:")
-    print(list[list_rand_num])
+        print("Computer chose:")
+        print(list[list_rand_num])
 
-    if list[selection] == list[list_rand_num]:
-        print("Draw  :S")
-    elif (list[selection] == rock and list[list_rand_num] == paper) or (list[selection] == paper and list[list_rand_num] == scissors) or (list[selection] == scissors and list[list_rand_num] == rock):
-        print("You lose :(")
-    elif (list[selection] == rock and list[list_rand_num] == scissors) or (list[selection] == paper and list[list_rand_num] == rock) or (list[selection] == scissors and list[list_rand_num] == paper):
-        print("You win :)")
-
-
+        if list[selection] == list[list_rand_num]:
+            print("Draw  :S\n")
+        elif (list[selection] == rock and list[list_rand_num] == paper) or (list[selection] == paper and list[list_rand_num] == scissors) or (list[selection] == scissors and list[list_rand_num] == rock):
+            print("You lose :(\n")
+        elif (list[selection] == rock and list[list_rand_num] == scissors) or (list[selection] == paper and list[list_rand_num] == rock) or (list[selection] == scissors and list[list_rand_num] == paper):
+            print("You win :)\n")
